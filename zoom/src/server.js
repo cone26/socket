9953eb797b98b1152691c86e4841481da95c9996
SiwonHwang <sight41siyah@gmail.com>
2022-09-05 19:56:50 +0900

    ws 서버 세팅

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
-
+import http from "http";
+import WebSocket from "ws";
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-const handleListen = () => console.log("Hello");
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
