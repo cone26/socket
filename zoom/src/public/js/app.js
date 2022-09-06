41b02287b8e5460605168d507f538279bb0102a3
SiwonHwang <sight41siyah@gmail.com>
2022-09-06 23:29:13 +0900

    닉네임과 메세지를 서버에서 구분해서 받기

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,21 +1,36 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nicknameForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 socket.addEventListener("open", () => {
   console.log("Connected to the Server ☑️");
 });
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerHTML = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from the Server ❌");
 });
+
 function handleSubmit(e) {
   e.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new message", input.value));
+  input.value = "";
+}
+function handleNickSubmit(e) {
+  e.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
   input.value = "";
 }
 messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNickSubmit);
