f493c32c323e03f0b462ffabd2de5d71fc55f911
SiwonHwang <sight41siyah@gmail.com>
2022-09-10 22:52:24 +0900

    video call camera select

--- videocall/src/public/app.js
+++ videocall/src/public/app.js
@@ -3,10 +3,26 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.querySelector("#mute");
 const cameraBtn = document.querySelector("#camera");
+const cameraSelect = document.querySelector("#cameras");
 
 let myStream;
 let muted = false;
 let cameraoff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 async function getMedia() {
   try {
@@ -15,11 +31,13 @@ async function getMedia() {
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
 }
 function handleMuteClick() {
+  myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -29,6 +47,7 @@ function handleMuteClick() {
   }
 }
 function handleCameraClick() {
+  myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
   if (cameraoff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraoff = false;
@@ -37,6 +56,10 @@ function handleCameraClick() {
     cameraoff = true;
   }
 }
+function handleCameraChange() {
+  console.log(cameraSelect.value);
+}
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+cameraSelect.addEventListener("input", handleCameraChange);
 getMedia();
