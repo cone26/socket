6be307783bd506615bebeb4738f5e25f5664fc82
SiwonHwang <sight41siyah@gmail.com>
2022-09-11 17:07:06 +0900

    videocall sender & local tunnel server

--- videocall/src/public/app.js
+++ videocall/src/public/app.js
@@ -76,7 +76,15 @@ function handleCameraClick() {
 }
 async function handleCameraChange() {
   await getMedia(cameraSelect.value);
+  if (myPeerConnection) {
+    const videoTack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection //
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTack);
+  }
 }
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 cameraSelect.addEventListener("input", handleCameraChange);
