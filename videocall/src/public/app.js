27cb1595399462a7256587d3026a7c2af0aa32f0
SiwonHwang <sight41siyah@gmail.com>
2022-09-11 16:05:47 +0900

    video call make offers

--- videocall/src/public/app.js
+++ videocall/src/public/app.js
@@ -14,6 +14,7 @@ let myStream;
 let muted = false;
 let cameraoff = false;
 let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -83,10 +84,11 @@ cameraSelect.addEventListener("input", handleCameraChange);
 // welcome
 const welcomeFrom = welcome.querySelector("form");
 
-function startMedia() {
+async function startMedia() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 function handleWelcomeSubmit(e) {
   e.preventDefault();
@@ -98,6 +100,19 @@ function handleWelcomeSubmit(e) {
 welcomeFrom.addEventListener("submit", handleWelcomeSubmit);
 
 //socket
-socket.on("welcome", () => {
-  console.log("someone joined");
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
 });
+
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+// RTC code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
--- videocall/src/server.js
+++ videocall/src/server.js
@@ -20,6 +20,9 @@ io.on("connection", (socket) => {
     done();
     socket.to(roomName).emit("welcome");
   });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
 });
 
 server.listen(3000, handleListen);
